a780854d4d7922b0978ba6ca42bf30c53fa49135
mark teng <mark_19475@hotmail.com>
2016-05-27 09:08:15 +0700

    User Login and Logout

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var text1 = "connect";
+var text2 = "disconnect";
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+
+
+io.on('connection', function(socket) {
+	socket.broadcast.emit('user connection', text1);
+	socket.on('disconnect',function(){
+		socket.broadcast.emit('user connection', text2);
+	});
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+
+io.on('connection', function(socket) {
+	socket.on('chat message', function(msg) {
+		io.emit('chat message', msg);
+	});
+});
+
+http.listen(3000, function() {
+	console.log('listening on *:3000');
 });
